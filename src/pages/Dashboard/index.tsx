2ec1b05a7c9aa7bbe25cb4d165d9be5059566aa6
augustomarsola <augusto.marsola@live.com>
2020-07-02 13:33:24 -0300

    📚 Completed the first step of style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,62 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button>Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="#a">
+          <img
+            src="https://avatars2.githubusercontent.com/u/25964446?s=460&u=ca42b8bd0f3eb1452a75bd0d97b4b0f991e7dc22&v=4"
+            alt="Avatar Augusto Marsola"
+          />
+          <div>
+            <strong>augustomarsola.github.io</strong>
+            <p>
+              Site para apresentação de projetos realizados por Augusto Marsola
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="#a">
+          <img
+            src="https://avatars2.githubusercontent.com/u/25964446?s=460&u=ca42b8bd0f3eb1452a75bd0d97b4b0f991e7dc22&v=4"
+            alt="Avatar Augusto Marsola"
+          />
+          <div>
+            <strong>augustomarsola.github.io</strong>
+            <p>
+              Site para apresentação de projetos realizados por Augusto Marsola
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="#a">
+          <img
+            src="https://avatars2.githubusercontent.com/u/25964446?s=460&u=ca42b8bd0f3eb1452a75bd0d97b4b0f991e7dc22&v=4"
+            alt="Avatar Augusto Marsola"
+          />
+          <div>
+            <strong>augustomarsola.github.io</strong>
+            <p>
+              Site para apresentação de projetos realizados por Augusto Marsola
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
